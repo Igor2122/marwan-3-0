7c28096f32d68b29fc362eb5470cfe77f20c6ddd
Igor <igor_pavlenko@yahoo.com>
2018-01-15 20:56:42 +0300

    updated the animations and div names

--- JQ/jsrecipie.js
+++ JQ/jsrecipie.js
@@ -1,41 +1,67 @@
-const helthydiv = document.getElementsByClassName('item')[0];
-const fastCooked = document.getElementsByClassName('item')[1];
-const dessert = document.getElementsByClassName('item')[2];
-const divOne = document.getElementById('item-11');
-const div = document.getElementsByTagName('button')[0];
-const divTwo = document.getElementById('item-12');
-const divThree = document.getElementById('item-13');
-
-helthydiv.addEventListener('click', () => {
-    divOne.style.transform = 'translateX(0%)';
-    divTwo.style.transform = 'translateX(100%)';
-    divThree.style.transform = 'translateX(100%)';
-    helthydiv.style.cursor = 'pointer';
-});
-
-fastCooked.addEventListener('click', () => {
-    divOne.style.transform = 'translateX(100%)';
-    divTwo.style.transform = 'translateX(0%)';
-    divThree.style.transform = 'translateX(100%)';
-});
-
-dessert.addEventListener('click', () => {
-    console.log('hello');
-    divOne.style.transform = 'translateX(100%)';
-    divTwo.style.transform = 'translateX(100%)';
-    divThree.style.transform = 'translateX(0%)';    
-});
 
 const topBackdrImg = document.getElementsByClassName('top-backdrop')[0];
 
 window.addEventListener('load', loadFunction);
 
-function loadFunction () {
+function loadFunction() {
     topBackdrImg.style.transition = "all 2s";
     topBackdrImg.style.right = '-10%';
 }
 
+const soupDiv  = document.getElementById('item-10');
+const healthyDiv = document.getElementById('item-11');
+const sandwichesDiv = document.getElementById('item-12');
+const maincoursediv = document.getElementById('item-13');
+const dessertDiv = document.getElementById('item-14');
 
- 
+const buttonEventParent = document.getElementsByClassName('items')[0];
+
+console.log(buttonEventParent);
+
+buttonEventParent.addEventListener('click', (event) => {
+   function showContent (elem) {
+       elem.style.transform = 'translateX(0)';
+   }
+
+   function hideContent (elem) {
+       elem.style.transform = 'translateX(100%)';
+   }
+
+    if(event.target.textContent === 'Soup' ) {
+        showContent(soupDiv);
+        hideContent(healthyDiv);
+        hideContent(sandwichesDiv);
+        hideContent(maincoursediv);
+        hideContent(dessertDiv);
+    } else if (event.target.textContent == 'Healthy') {
+        showContent(healthyDiv);
+        hideContent(soupDiv);
+        hideContent(sandwichesDiv);
+        hideContent(maincoursediv);
+        hideContent(dessertDiv);
+    } else if (event.target.textContent == 'Sandwiches') {
+        showContent(sandwichesDiv);
+        hideContent(soupDiv);
+        hideContent(healthyDiv);
+        hideContent(maincoursediv);
+        hideContent(dessertDiv);
+    } else if (event.target.textContent === 'Main Courses') {
+        showContent(maincoursediv);
+        hideContent(soupDiv);
+        hideContent(healthyDiv);
+        hideContent(sandwichesDiv);
+        hideContent(dessertDiv);
+    } else if (event.target.textContent === 'Dessert'){
+        showContent(dessertDiv);
+        hideContent(soupDiv);
+        hideContent(healthyDiv);
+        hideContent(sandwichesDiv);
+        hideContent(maincoursediv);
+    }
+});
+
+
+
+
 
 
