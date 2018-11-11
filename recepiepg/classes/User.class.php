de1074fecfc3c71a79b715fe4bea51742ae06db6
Igor <igor_pavlenko@yahoo.com>
2018-11-11 13:56:16 +0000

    converted userAuthentication to User method

--- recepiepg/classes/User.class.php
+++ recepiepg/classes/User.class.php
@@ -16,5 +16,23 @@ class User extends RecDB
             return $stmt->fetchAll();
     }
     
+    function userAuthentication($user, $user_id, $user_pwd){
+        
+        
+        
+        foreach($user as $user){
+
+        if($user_id == $user['user_email'] && $user_pwd == $user['user_pwd']){
+            header('Location: https://marwan-3-0-updated-recepies-igdevelopers.c9users.io/recepiepg/recepies.php');
+            exit();
+        } else {
+            echo 'Wrong login details';
+            // header("Location: recepiepg/index.php");
+           
+        }
+    
+    }
+    }
+    
     // user_first, user_last, user_email, user_uid, user_pwd
 }
