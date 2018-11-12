7d7612ff9aa97266f830654b680a21206987ed99
Igor <igor_pavlenko@yahoo.com>
2018-11-12 21:41:52 +0300

    rct bse

--- recepiepg/react/project/src/App.js
+++ recepiepg/react/project/src/App.js
@@ -1,32 +1,12 @@
 import React, { Component } from 'react';
-import Projects from './components/Projects'; 
 import './App.css';
 
+
 class App extends Component {
-  constructor (){
-    super();
-    this.state = {
-      projects: [
-        {
-          title: 'Business Website', 
-          category: 'Web Desing'
-        },
-        {
-          title: 'Social App', 
-          category: 'Mobile Develpment'
-        },
-        {
-          title: 'E commerce Shopping Cart', 
-          category: 'Web Develpment'
-        }
-        ]
-    };
-  }
   render() {
     return (
       <div className="App">
-        <h1>My app</h1>
-        <Projects projects={this.state.projects}/>
+        <h1>Hi I'm a React App</h1>
       </div>
     );
   }
