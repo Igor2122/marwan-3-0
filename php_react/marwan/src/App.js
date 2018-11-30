519c7fcb787f3a608690f8e40246416127a9cbc1
Igor <igor_pavlenko@yahoo.com>
2018-11-30 18:09:57 +0300

    fetching data from api created in php

--- php_react/marwan/src/App.js
+++ php_react/marwan/src/App.js
@@ -7,20 +7,35 @@ import Layout from './containers/Layout/Layout';
 import { BrowserRouter } from 'react-router-dom'
 
 class App extends Component {
-  
-  
-  componentDidMount (){
+
+  state = {
+    recep: []
+  }
+
+  componentDidMount () {
     
-         axios.get('/../../queryData/load-recepies.php')
-        .then(response => {
-            console.log(response);
+    axios.get('http://localhost:8888/marwan4.0/php_react/api/Recepie/read.php')
+    .then(response => {
+      console.log(typeof(response.data));
+      this.setState({recep: response.data});
+      
+    });
   }
-  }
-  
   
   render() {
+
+    // const recep = this.state.recep.map(post => {
+    //   return {
+
+    //   }
+    // }
+
+    // )
+
     return(
-      
+      <div>
+
+      </div>
       
     );
     
