c61d37f53255e7e55af0b08e675e8a52a5cf4830
Igor <igor_pavlenko@yahoo.com>
2018-11-30 20:30:33 +0300

    ouputting images with posts

--- php_react/marwan/src/App.js
+++ php_react/marwan/src/App.js
@@ -2,9 +2,13 @@
 import React, { Component } from 'react';
 // eslint-disable-next-line
 import axios from 'axios';
+// eslint-disable-next-line
 import classes from './App.css';
+// eslint-disable-next-line
 import { BrowserRouter } from 'react-router-dom'
-import Recep from './components/Recep';
+import Recep from './components/Recepie/Recep';
+
+
 
 class App extends Component {
 
@@ -13,30 +17,34 @@ class App extends Component {
   }
 
   componentDidMount () {
-    
     axios.get('http://localhost:8888/marwan4.0/php_react/api/Recepie/read.php')
     // axios.get('https://jsonplaceholder.typicode.com/posts')
     .then(response => {
       console.log(response.data, typeof(response.data));
       this.setState({recep: response.data.recepies});
-      
     });
   }
   
   render() {
-
     const recepies = this.state.recep.map(post => {
       return <Recep 
               key={post.img_id}
-              name={post.name} />
+              name={post.name} 
+              img={post.image}
+              />
     }
     )
 
     return(
       <div className="row">
-        {recepies}
+        <div className="col-md-3"></div>
+        <div className="col-md-9">
+          <div className="d-flex justify-content-around">
+            {recepies}
+          </div>         
+        </div>
       </div>
-      
+
     );
     
   }
--- php_react/marwan/src/components/Recepie/Recep.js
+++ php_react/marwan/src/components/Recepie/Recep.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import classes from './Recep.css'
+
 const recep = (props) => {
 return (
 
    
       <div className={[classes.Card, "card"].join(' ')} id={props.id}>
-         <img className="card-img-top" src="..." alt="Card image cap"></img>
+         <img className="card-img-top" src={require(`../../assets/img/${props.img}`)} alt="Card image cap"></img>
          <div className="card-body">
             <h5 className="card-title">{props.name}</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
