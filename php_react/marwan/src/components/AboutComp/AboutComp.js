af636e4c50991f92c9e1fd285144524ebc617baf
Igor <igor_pavlenko@yahoo.com>
2018-12-23 14:23:47 +0000

    loading images via img npm

---
+++ php_react/marwan/src/components/AboutComp/AboutComp.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import classes from './AboutComp.css';
+import linkdnLogo from '../../assets/svg/linkedin.svg';
+import Img from 'react-image'
+
+const aboutComp =() => {
+        return (
+            <Container className={classes.Section}>
+                <Row>
+                  <Col xs="4">
+                    <div className={classes.Tagline}>
+                        <h1>Meet the Chef</h1>
+                        <p>Experienced Executive Chef with a demonstrated history of working in the hospitality industry. Motivated and enthusiastic Chef who has a real passion for food, and who is sure to exceed your customer’s expectations.</p>
+                        <Row>
+                          <Col xs="4"></Col>
+                          <Col xs="auto"><a href="https://www.cssmatic.com/gradient-generator#'\-moz\-linear\-gradient\%28left\%2C\%20rgba\%2863\%2C54\%2C42\%2C1\%29\%200\%25\%2C\%20rgba\%2821\%2C18\%2C14\%2C1\%29\%20100\%25\%29\%3B'">My Linkedin</a></Col>
+                          <Col xs="4">
+                          <Img width="35px" src={[
+                            linkdnLogo,
+                            'https://www.example.com/bar.jpg'
+                            ]}
+                            
+                          />
+                          </Col>
+                        </Row>    
+                    </div>
+                    
+                  </Col>
+                  <Col xs="4">.col-auto - variable width content</Col>
+                  <Col xs="4">.col-3</Col>
+                </Row>
+            </Container>
+        );
+}
+
+export default aboutComp;
--- php_react/marwan/src/containers/About/About.js
+++ php_react/marwan/src/containers/About/About.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import Classes from './About.css';
+import AboutComp from '../../components/AboutComp/AboutComp';
 
 class About extends Component {
     render () {
         return (
             <div className={Classes.About}>
-                <h1>About Section</h1>
+                <AboutComp />
             </div>
         );
     }
