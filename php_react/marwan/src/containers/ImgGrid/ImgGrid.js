d73120b82d09af6bf17baed8509309a1b16ae4b9
Igor <igor_pavlenko@yahoo.com>
2018-12-25 12:25:55 +0000

    save before ract lightbox implelemtation

--- php_react/marwan/src/containers/ImgGrid/ImgGrid.js
+++ php_react/marwan/src/containers/ImgGrid/ImgGrid.js
@@ -13,7 +13,7 @@ import Spinner from '../../components/UI/Spinner/Spinner'
 class Recepie extends Component {
 
   state = {
-    images: ['https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    imagesThumb: ['https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     'https://images.pexels.com/photos/210243/pexels-photo-210243.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     'https://images.pexels.com/photos/1562/italian-landscape-mountains-nature.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     'https://images.pexels.com/photos/355241/pexels-photo-355241.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'],
@@ -21,27 +21,27 @@ class Recepie extends Component {
   }
 
   componentDidMount() {
-    // 
+    
     // axios.get('http://localhost:8888/marwan4.0/php_react/api/Recepie/read.php')
     // axios.get('https://jsonplaceholder.typicode.com/posts')
 
 
-    // axios.get('https://api.instagram.com/v1/users/self/media/recent/?access_token=285502478.8001032.48720ae4588d48c9be566f37274a24ff')
-    //   .then(response => {
-    //     console.log(response.data, typeof(response.data));
-    //     let images = []
-    //     for (const key of response.data.data) {
-    //       images.push(key['images']['standard_resolution']['url']);
-    //       console.log(images);
-    //     }
-    //     this.setState({ images: images })
-    //   })
-    //   .catch(error => {
-    //     // console.log(error);
-    //     this.setState({ error: true });
-    //   });;
+    axios.get('https://api.instagram.com/v1/users/self/media/recent/?access_token=285502478.8001032.48720ae4588d48c9be566f37274a24ff')
+      .then(response => {
+        console.log(response.data, typeof(response.data));
+        let images = []
+        for (const key of response.data.data) {
+          images.push(key['images']['low_resolution']['url']);
+          console.log(key);
+        }
+        this.setState({ imagesThumb: images })
+      })
+      .catch(error => {
+        // console.log(error);
+        this.setState({ error: true });
+      });;
 
-    // console.log(this.state.images);
+    console.log(this.state.images);
 
   }
 
@@ -56,23 +56,23 @@ class Recepie extends Component {
             
             <div className={classes.DviOne}>
               <Overlay />
-                <Image max-width="100%" height="100%" src={this.state.images[0]}
+                <Image max-width="100%" height="100%" src={this.state.imagesThumb[0]}
                 loader={<Spinner />} />
             </div>
             
             <div className={classes.DivTwo}>    
               <Overlay />
-                <Image max-width="100%" height="100%" src={this.state.images[1]}
+                <Image max-width="100%" height="100%" src={this.state.imagesThumb[1]}
                 loader={<Spinner />} />
             </div>
 
             <div className={classes.DivThree}>
-              <Image max-width="100%" height="100%" src={this.state.images[2]}
+              <Image max-width="100%" height="100%" src={this.state.imagesThumb[2]}
                 loader={<Spinner />} />
             </div>
             
             <div className={classes.DivFour}>
-              <Image max-width="100%" height="100%" src={this.state.images[3]}
+              <Image max-width="100%" height="100%" src={this.state.imagesThumb[3]}
                 loader={<Spinner />} />
             </div>
             
