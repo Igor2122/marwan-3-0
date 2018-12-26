5b3153cdd6403a036c7f581d96becefee8bf3f83
igor2122 <igor_pavlenko@yahoo.com>
2018-12-26 11:25:41 +0300

    modal added

--- php_react/marwan/src/containers/ImgGrid/ImgGrid.js
+++ php_react/marwan/src/containers/ImgGrid/ImgGrid.js
@@ -16,7 +16,7 @@ class Recepie extends Component {
     imagesThumb: ['https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     'https://images.pexels.com/photos/210243/pexels-photo-210243.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     'https://images.pexels.com/photos/1562/italian-landscape-mountains-nature.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    'https://images.pexels.com/photos/355241/pexels-photo-355241.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'],
+    'https://images.pexels.com/photos/355241/pexels-photo-355241.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260','https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260','https://images.pexels.com/photos/807598/pexels-photo-807598.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260','https://images.pexels.com/photos/302804/pexels-photo-302804.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260','https://images.pexels.com/photos/371589/pexels-photo-371589.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260','https://images.pexels.com/photos/709552/pexels-photo-709552.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',],
     error: false,
   }
 
@@ -26,72 +26,74 @@ class Recepie extends Component {
     // axios.get('https://jsonplaceholder.typicode.com/posts')
 
 
-    axios.get('https://api.instagram.com/v1/users/self/media/recent/?access_token=285502478.8001032.48720ae4588d48c9be566f37274a24ff')
-      .then(response => {
-        console.log(response.data, typeof(response.data));
-        let images = []
-        for (const key of response.data.data) {
-          images.push(key['images']['standard_resolution']['url']);
-          console.log(key);
-        }
-        this.setState({ imagesThumb: images })
-      })
-      .catch(error => {
-        // console.log(error);
-        this.setState({ error: true });
-      });;
+    // axios.get('https://api.instagram.com/v1/users/self/media/recent/?access_token=285502478.8001032.48720ae4588d48c9be566f37274a24ff')
+    //   .then(response => {
+    //     console.log(response.data, typeof(response.data));
+    //     let images = []
+    //     for (const key of response.data.data) {
+    //       images.push(key['images']['standard_resolution']['url']);
+    //       console.log(key);
+    //     }
+    //     this.setState({ imagesThumb: images })
+    //   })
+    //   .catch(error => {
+    //     // console.log(error);
+    //     this.setState({ error: true });
+    //   });;
 
-    console.log(this.state.images);
+    // console.log(this.state.images);
 
   }
 
   render() {
 
+    const gridClasses = ['DviOne', 'DivTwo', 'DivThree', 'DivFour', 'DivFive', 'DivSix', 'DivSeven', 'DivEigtht', 'DivNine' ]
+    
     return (
 
       <div className={classes.GalleryGrid}>
             <div className={[classes.DviOne, classes.AllImages].join(' ')}>
               {/* <Overlay /> */}
-                <Image   src={this.state.imagesThumb[0]}
+                <Image className={classes.Image}   src={this.state.imagesThumb[0]}
                 loader={<Spinner />} />
             </div>
             
             <div className={[classes.DivTwo, classes.AllImages].join(' ')}>    
               <Overlay />
-                <Image   src={this.state.imagesThumb[1]}
+                <Image className={classes.Image}   src={this.state.imagesThumb[1]}
                 loader={<Spinner />} />
             </div>
 
             <div className={[classes.DivThree, classes.AllImages].join(' ')}>
-              <Image   src={this.state.imagesThumb[2]}
+              <Image className={classes.Image}   src={this.state.imagesThumb[2]}
                 loader={<Spinner />} />
             </div>
             
             <div className={[classes.DivFour, classes.AllImages].join(' ')}>
-              <Image   src={this.state.imagesThumb[3]}
+              <Image className={classes.Image}   src={this.state.imagesThumb[3]}
                 loader={<Spinner />} />
             </div>
             
             <div className={[classes.DivFive, classes.AllImages].join(' ')}>
-            <Image   src={this.state.imagesThumb[4]}
+            <Image className={classes.Image}   src={this.state.imagesThumb[4]}
                 loader={<Spinner />} />
             </div>
             
             <div className={[classes.DivSix, classes.AllImages].join(' ')}>
-            <Image   src={this.state.imagesThumb[5]}
+            <Image className={classes.Image}   src={this.state.imagesThumb[5]}
                 loader={<Spinner />} />
             </div>
             
             <div className={[classes.DivSeven, classes.AllImages].join(' ')}>  
-            <Image   src={this.state.imagesThumb[6]}
+            <Image className={classes.Image}   src={this.state.imagesThumb[6]}
                 loader={<Spinner />} />
             </div>
             <div className={[classes.DivEigtht, classes.AllImages].join(' ')}>  
-            <Image   src={this.state.imagesThumb[7]}
+            <Image className={classes.Image}   src={this.state.imagesThumb[7]}
                 loader={<Spinner />} />
             </div>
             <div className={[classes.DivNine, classes.AllImages].join(' ')}>  
-            <Image   src={this.state.imagesThumb[8]}
+            <Image className={classes.Image}   src={this.state.imagesThumb[8]}
                 loader={<Spinner />} />
             </div>
            
