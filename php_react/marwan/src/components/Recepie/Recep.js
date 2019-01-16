fad930216fdb82252d55bf8a545f1ca8257ce64c
Igor <igor_pavlenko@yahoo.com>
2019-01-16 21:07:34 +0100

    rendering fetched results to recepies done

--- php_react/marwan/src/components/Recepie/Recep.js
+++ php_react/marwan/src/components/Recepie/Recep.js
@@ -14,7 +14,7 @@ const Recepie = (props) => {
       <Card className={classes.Card}>
         <CardImg top width="100%" src="/img/tram.jpeg" alt="Card image cap"/>
         <CardBody>
-          <CardTitle>Card Title</CardTitle>
+          <CardTitle>{props.name}</CardTitle>
           <CardText>This is a wider card with supporting text below as a natural lead-in
             to additional content. This content is a little bit longer.</CardText>
           <CardText>
--- php_react/marwan/src/containers/websitePages/Recepies/Recepies.js
+++ php_react/marwan/src/containers/websitePages/Recepies/Recepies.js
@@ -1,19 +1,44 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import Rcepie from '../../../components/Recepie/Recep';
 import classes from './Recepies.css';
+import axios from 'axios';
+import Recepie from '../../../components/Recepie/Recep';
 
 class RecepiesPage extends Component {
 
+  state = {
+    recepies: null
+  }
 
-     render () {
-         return(
-          <div className={classes.RecepiesDiv}>
-               <h1>Recepies page</h1>
-               <Rcepie />
-          </div>
-         );
-    }
+  componentDidMount() {
+    axios
+      .get('https://marwannew-c69d8.firebaseio.com/recipes.json')
+      .then(response => {
+        console.log(response);
+        this.setState({recepies: response.data})
+      });
+  }
+
+  render() {
+
+       let recepie = null;
+     if(this.state.recepies){
+          let all = Object.keys(this.state.recepies);
+          console.log(all);
+          // Object.values(this.state.recepies)
+          // .map(rec => {
+          //      console.log(rec)
+          //      recepie = <Recepie name={rec.name} />
+          // })
      }
 
+    return (
+      <div className={classes.RecepiesDiv}>
+        <h1>Suggested Recepies:</h1>
+        {recepie}
+      </div>
+    );
+  }
+}
 
-export default RecepiesPage;        
+export default RecepiesPage;
