72ff03845f5a54c625a7e26792bf00fd3500eba8
Igor <igor_pavlenko@yahoo.com>
2019-01-16 20:37:17 +0100

    rencdring the recepie card on the page done

--- php_react/marwan/src/components/Recepie/Recep.js
+++ php_react/marwan/src/components/Recepie/Recep.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import {
-  Card,
-  CardBody,
-//   Button,
+  Card, CardBody,
+  //   Button,
   CardTitle,
   CardText,
   CardImg
 } from 'reactstrap';
+import classes from './Recep.css';
 
-const Example = (props) => {
+const Recepie = (props) => {
   return (
     <div>
-      <Card>
-        <CardImg
-          top
-          width="100%"
-          src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-          alt="Card image cap"/>
+      <Card className={classes.Card}>
+        <CardImg top width="100%" src="/img/tram.jpeg" alt="Card image cap"/>
         <CardBody>
           <CardTitle>Card Title</CardTitle>
           <CardText>This is a wider card with supporting text below as a natural lead-in
@@ -25,24 +21,9 @@ const Example = (props) => {
             <small className="text-muted">Last updated 3 mins ago</small>
           </CardText>
         </CardBody>
-      </Card>
-      <Card>
-        <CardBody>
-          <CardTitle>Card Title</CardTitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.</CardText>
-          <CardText>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </CardText>
-        </CardBody>
-        <CardImg
-          bottom
-          width="100%"
-          src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-          alt="Card image cap"/>
       </Card>
     </div>
   );
 };
 
-export default Example;
+export default Recepie;
--- php_react/marwan/src/containers/websitePages/Recepies/Recepies.js
+++ php_react/marwan/src/containers/websitePages/Recepies/Recepies.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import Rcepie from '../../../components/Recepie/Recep';
+import classes from './Recepies.css';
 
 class RecepiesPage extends Component {
 
 
      render () {
          return(
-          <div>
+          <div className={classes.RecepiesDiv}>
                <h1>Recepies page</h1>
                <Rcepie />
           </div>
